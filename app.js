884f5ed9a92a1796f49b797d57db95cc15f7329c
Jakub Tomas <tomasjakub21@gmail.com>
2020-02-26 21:54:47 +0100

    "add som functions and create Object for data from index "

--- app.js
+++ app.js
@@ -20,6 +20,32 @@ var budgetController = (function () {
 
 // UI Controller
 var UIController = (function () {
+// other controler
+
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add_description",
+        inputValue: "add_value ",
+        inputButton :".add_btn"
+    };
+
+    return { // public method
+        getinput: function () {// data from user interface
+
+            return {// we have the value from classs add type
+                //will be either inc or exp
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+                // three input value  we return 3 values same time we return object with 3 properties
+            };
+        },
+
+        getDOMstring: function () {
+            return DOMstrings; // return object DOMstrings
+        }
+    }
+
 
 })();
 
@@ -27,29 +53,40 @@ var UIController = (function () {
 /// Global app controller
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstring;// we have data in controller
 
-    document.querySelector(".add__btn").addEventListener(
-        "click", function () {
-            //     console.log("The button click");
+    var ctrlAddItem = function () {
+        //     console.log("The button click");
 
-            // 1.Get the file input data
-            // 2. Add the item to the budget controller
+        // 1.Get the file input data
+        // 2. Add the item to the budget controller
 
-            // 3. Add the item to the UI
-            // 4. Calculate the budget
-            // 5 . Dispalay th ebudget ont the UI
-        });
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5 . Dispalay th ebudget ont the UI
+        // console.log("its work");
 
-    document.addEventListener("keypress" , function (event) {
+        var input = UICtrl.getinput();
+        console.log(input);
+
+
+    };
+
+    document.querySelector(DOM.inputButton).addEventListener(
+        "click", ctrlAddItem);// after click call the function ctrlAddItem
+
+    document.addEventListener("keypress", function (event) {
         //console.log(event);
 
-        if (event.keyCode ===13 || event.which ===13) {
+        if (event.keyCode === 13 || event.which === 13) {
             //console.log("Enter was pressed");
+            ctrlAddItem();
 
-        }// the end 2>39
+        }
+ // 2 :56 : 52
 
-        
     });
 
 })(budgetController, UIController); // ma pristup k ostatnym modulom
 // pretoze su v inych scope
+// 2:41 how to take data fro from different  html input ypes
